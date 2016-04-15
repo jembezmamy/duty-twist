4c98c7fca13bec772c8a3523ca51c4263a7c0426
Paweł <jembezmamy@users.noreply.github.com>
2016-04-15 16:25:50 +0200

    home page fixes

--- app/components/schedule-summary/component.js
+++ app/components/schedule-summary/component.js
@@ -3,9 +3,12 @@ import RippleEffect from "../ripple-effect/mixin";
 
 export default Ember.Component.extend(RippleEffect, {
   classNames: ["schedule-summary", "card"],
+  classNameBindings: ["isLoading"],
 
   click(e) {
     this._super(e);
     this.sendAction("show", this.get("model"));
-  }
+  },
+
+  isLoading: Ember.computed.not("model.isFulfilled")
 });
--- app/schedules/index/view.js
+++
@@ -1,8 +0,0 @@
-import Ember from "ember";
-
-export default Ember.Component.extend({
-  classNames: "schedules-index-view",
-  classNameBindings: ["isEmpty"],
-
-  isEmpty: Ember.computed.equal("model.length", 0)
-});
